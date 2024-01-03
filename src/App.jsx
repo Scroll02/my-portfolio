49a2800f8a86bbe6e75a0a25cfad548ccadcbabd
Christian Joel Banquito <cjbanquito@gmail.com>
2024-01-03 22:32:46 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,249 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import "boxicons";
+import ScrollReveal from "scrollreveal";
+import { techStackData, projectsData } from "./data";
+import ichanImg from "./assets/images/ichan.png";
+
+function App() {
+  const [expandedCards, setExpandedCards] = useState([]);
+  const toggleExpand = (projectId) => {
+    setExpandedCards((prevExpandedCards) =>
+      prevExpandedCards.includes(projectId)
+        ? prevExpandedCards.filter((id) => id !== projectId)
+        : [...prevExpandedCards, projectId]
+    );
+  };
+
+  useEffect(() => {
+    // -----TOGGLE NAV MENU FUNCTION-----
+    function toggleNavMenu() {
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menu");
+
+      // add/toggle class ".active" to trigger the ".nav-menu.active" with a "display: block" style
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    }
+
+    // -----CLOSE NAV MENU FUNCTION-----
+    function closeNavMenu() {
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menu");
+
+      // remove the ".active" class to revert to the original state of ".nav-menu" with a "display: none" style
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }
+
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    hamburger.addEventListener("click", toggleNavMenu);
+
+    // Close the nav-menu when a nav-link is clicked
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", closeNavMenu);
+    });
+
+    return () => {
+      hamburger.removeEventListener("click", toggleNavMenu);
+      navLinks.forEach((navLink) => {
+        navLink.removeEventListener("click", closeNavMenu);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    ScrollReveal({
+      reset: true,
+      distance: "50px",
+      duration: 2000,
+      delay: 200,
+    });
+    ScrollReveal().reveal(".container, footer", {
+      origin: "top",
+    });
+  }, []);
+
+  return (
+    <>
+      <header>
+        <div className="logo">
+          {/* <img src={logoImg} alt="logo" /> */}
+          <h1>Ichan</h1>
+        </div>
+        <nav className="nav-bar">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <a href="#about" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#techStack" className="nav-link">
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#projects" className="nav-link">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          <div className="hamburger">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        {/* -----ABOUT SECTION----- */}
+        <section className="container" id="about">
+          <div className="content">
+            <div className="about">
+              <h1>
+                Hi, I'm <span>Ichan</span>, Aspiring Web Developer
+              </h1>
+              <p>
+                I create sleek and user-friendly websites that blend form and
+                function seamlessly. With a passion for coding and a knack for
+                problem-solving, I bring a fresh perspective to web development.
+              </p>
+            </div>
+            <div className="image">
+              <img src={ichanImg} alt="ichan" />
+            </div>
+          </div>
+        </section>
+
+        {/* -----TECH STACK SECTION----- */}
+        <section className="container" id="techStack">
+          <h1 className="title">Tech Stack</h1>
+          <div className="skills">
+            {techStackData.map((tech) => (
+              <div className="tech" key={tech.id}>
+                <img src={tech.imgSrc} alt={tech.alt} />
+                <h5>{tech.title}</h5>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        {/* -----PROJECTS SECTION----- */}
+        <section className="container" id="projects">
+          <h1 className="title">Projects</h1>
+          <div className="wrapper">
+            {projectsData.map((proj) => (
+              <div
+                className={`card ${
+                  expandedCards.includes(proj.id) ? "expanded" : ""
+                }`}
+                key={proj.id}
+              >
+                <img src={proj.img} alt={proj.alt} />
+                <h3>{proj.title}</h3>
+                <div className="tech-stack">
+                  {proj.techStack.map((techImg, index) => (
+                    <img key={index} src={techImg} alt={`tech-${index}`} />
+                  ))}
+                </div>
+                <p>
+                  {expandedCards.includes(proj.id)
+                    ? proj.desc
+                    : `${proj.desc.slice(0, 150)}...`}
+                  {proj.desc.length > 150 && (
+                    <a onClick={() => toggleExpand(proj.id)}>
+                      {expandedCards.includes(proj.id)
+                        ? "See less"
+                        : "See more"}
+                    </a>
+                  )}
+                </p>
+                <a
+                  className="btn-link"
+                  href={proj.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {proj.btnTitle}
+                  <box-icon
+                    name="link-external"
+                    color="white"
+                    size="16px"
+                  ></box-icon>
+                </a>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        {/* -----FOOTER SECTION----- */}
+        <footer id="contact">
+          <div className="contact-info">
+            <h1 className="title">Get in Touch</h1>
+            <p>
+              Have a question, want to share thoughts, or just fancy a chat?
+              Feel free to drop me a message anytime.
+            </p>
+            <a href="mailto:banquitocj@gmail.com">
+              <box-icon name="send" color="white"></box-icon>Send a message
+            </a>
+          </div>
+          <div className="social-icons">
+            <a
+              href="https://www.facebook.com/ichan04/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <box-icon
+                color="white"
+                name="facebook"
+                type="logo"
+                animation="tada-hover"
+                size="35px"
+              ></box-icon>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/christian-joel-banquito-28178b262/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <box-icon
+                color="white"
+                name="linkedin"
+                type="logo"
+                animation="tada-hover"
+                size="35px"
+              ></box-icon>
+            </a>
+            <a
+              href="https://github.com/Scroll02"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <box-icon
+                color="white"
+                name="github"
+                type="logo"
+                animation="tada-hover"
+                size="35px"
+              ></box-icon>
+            </a>
+          </div>
+          <p className="copyright">2023 | Christian Joel Banquito</p>
+        </footer>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,90 @@
+// -----Tech Stack Data-----
+import htmlImg from "./assets/images/html5.png";
+import cssImg from "./assets/images/css.png";
+import sassImg from "./assets/images/sass.png";
+import bootstrapImg from "./assets/images/bootstrap.png";
+import javascriptImg from "./assets/images/javascript.png";
+import reactImg from "./assets/images/react.png";
+import reduxImg from "./assets/images/redux.png";
+import androidStudioImg from "./assets/images/android-studio.png";
+import visualStudioCodeImg from "./assets/images/visual-studio.png";
+import firebaseImg from "./assets/images/firebase.png";
+import wordpressImg from "./assets/images/wordpress.png";
+import postmanImg from "./assets/images/postman.png";
+import paymongoImg from "./assets/images/paymongo.png";
+
+export const techStackData = [
+  { id: 1, imgSrc: htmlImg, alt: "HTML", title: "HTML" },
+
+  { id: 2, imgSrc: cssImg, alt: "CSS", title: "CSS" },
+
+  { id: 3, imgSrc: sassImg, alt: "Sass", title: "Sass" },
+
+  { id: 4, imgSrc: bootstrapImg, alt: "Bootstrap", title: "Bootstrap" },
+
+  { id: 5, imgSrc: javascriptImg, alt: "Javascript", title: "Javascript" },
+
+  { id: 6, imgSrc: reactImg, alt: "React", title: "React" },
+
+  { id: 7, imgSrc: reduxImg, alt: "Redux", title: "Redux" },
+
+  { id: 8, imgSrc: firebaseImg, alt: "Firebase", title: "Firebase" },
+
+  { id: 9, imgSrc: wordpressImg, alt: "Wordpress", title: "Wordpress" },
+
+  { id: 10, imgSrc: postmanImg, alt: "Postman", title: "Postman" },
+
+  {
+    id: 11,
+    imgSrc: androidStudioImg,
+    alt: "Android Studio",
+    title: "Android Studio",
+  },
+
+  {
+    id: 12,
+    imgSrc: visualStudioCodeImg,
+    alt: "Visual Studio Code",
+    title: "Visual Studio Code",
+  },
+];
+
+// -----Projects Data-----
+import homeWebImg from "./assets/images/home-web.jpg";
+import dashboardImg from "./assets/images/dashboard.jpg";
+import riderAppImg from "./assets/images/roseG-rider.png";
+
+export const projectsData = [
+  {
+    id: 1,
+    img: homeWebImg,
+    alt: "Rose-G Website",
+    title: "Rose-G Website",
+    desc: "The Rose-G E-Commerce Website lets you effortlessly check out and order your preferred food. It's easy to use, safe for payments, and guarantees a smooth online ordering experience, delivering Rose-G's delicious offerings right to your door.",
+    techStack: [reactImg, reduxImg, firebaseImg, cssImg, paymongoImg],
+    btnTitle: "View",
+    url: "https://www.rosegspecialpalabok.com",
+  },
+
+  {
+    id: 2,
+    img: dashboardImg,
+    alt: "Rose-G Admin",
+    title: "Rose-G Admin",
+    desc: "The Rose-G E-Commerce Admin is like the command center for the website. It helps the owner manage things, like updating the website and keeping track of user orders for the Rose-G E-Commerce Website.",
+    techStack: [reactImg, reduxImg, firebaseImg, sassImg],
+    btnTitle: "View",
+    url: "https://rose-g-admin-7f049.web.app",
+  },
+
+  {
+    id: 3,
+    img: riderAppImg,
+    alt: "Rose-G Driver",
+    title: "Rose-G Driver App",
+    desc: "The Rose-G Driver App helps Rose-G's delivery drivers by showing them important details about user orders. It includes everything from what to deliver to any special instructions, making sure drivers can deliver Rose-G orders smoothly.",
+    techStack: [reactImg, reduxImg, firebaseImg, cssImg, androidStudioImg],
+    btnTitle: "Download APK",
+    url: "https://drive.google.com/file/d/1N3aPliScDDskWmVwg6JvJK72EXE1zgeD/view?usp=sharing",
+  },
+];
